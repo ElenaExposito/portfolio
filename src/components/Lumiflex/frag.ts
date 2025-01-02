ae1eec5d430bb1eb8899a7ae67d16b27257e0157
Elena Expósito <elexla97@gmail.com>
2025-01-02 23:22:53 +0100

    Refactor main view

---
+++ src/components/Lumiflex/frag.ts
@@ -0,0 +1,14 @@
+const frag = `
+precision highp float;
+
+uniform float uTime;
+uniform vec3 uColor;
+
+varying vec2 vUv;
+
+void main() {
+    gl_FragColor.rgb = 0.5 + 0.3 * cos(vUv.xyx + uTime) + uColor;
+    gl_FragColor.a = 1.0;
+}
+`;
+export default frag;
---
+++ src/components/Lumiflex/index.tsx
@@ -0,0 +1,77 @@
+import { Renderer, Program, Mesh, Color, Triangle } from "ogl";
+import { useEffect, useRef } from "react";
+import vert from "./vert.ts";
+import frag from "./frag.ts";
+
+export function Lumiflex() {
+  const ctnDom = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ctnDom.current) {
+      return;
+    }
+
+    const ctn = ctnDom.current;
+    const renderer = new Renderer();
+    const gl = renderer.gl;
+    gl.clearColor(1, 1, 1, 1);
+
+    function resize() {
+      if (ctn == null) {
+        return;
+      }
+
+      const scale = 1;
+      renderer.setSize(ctn.offsetWidth * scale, ctn.offsetHeight * scale);
+    }
+    window.addEventListener("resize", resize, false);
+    resize();
+
+    const geometry = new Triangle(gl);
+
+    const program = new Program(gl, {
+      vertex: vert,
+      fragment: frag,
+      uniforms: {
+        uTime: { value: 0 },
+        uColor: { value: new Color(0.3, 0.2, 0.5) },
+      },
+    });
+
+    const mesh = new Mesh(gl, { geometry, program });
+
+    let animateId: number;
+
+    animateId = requestAnimationFrame(update);
+
+    function update(t: number) {
+      animateId = requestAnimationFrame(update);
+
+      const time = t * 0.01,
+        speed = 1.0;
+
+      program.uniforms.uTime.value = time * speed * 0.1;
+
+      renderer.render({ scene: mesh });
+    }
+
+    ctn.appendChild(gl.canvas);
+    return () => {
+      cancelAnimationFrame(animateId);
+      window.removeEventListener("resize", resize);
+      ctn.removeChild(gl.canvas);
+      gl.getExtension("WEBGL_lose_context")?.loseContext();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={ctnDom}
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+      }}
+    />
+  );
+}
