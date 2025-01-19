44d3a0df3acc5955b2963dfe64c5630350f562b8
Elena Expósito <elexla97@gmail.com>
2025-01-19 17:02:01 +0100

    Change some files and adapt content for build

---
+++ src/data/data-about/formacion-complementaria.ts
@@ -0,0 +1,44 @@
+import ResponsiveWebDesignImage from "../../assets/images/Freecodecamp-Responsive-Web-Design.png";
+import JavaScriptImage from "../../assets/images/Freecodecamp-JavaScript.png";
+import AcepcorImage from "../../assets/images/acepcor.png";
+import GoogleActivate1Image from "../../assets/images/intro-des-web-google-activate-1.png";
+import GoogleActivate2Image from "../../assets/images/intro-des-web-google-activate-2.png";
+
+export const formacionComplementaria = [
+  {
+    id: "4",
+    title: "Responsive Web Design",
+    institution: "freeCodeCamp",
+    date: "Enero 2024",
+    hours: 300,
+    knowledge: ["Responsive Design", "CSS3", "HTML5", "Principios de diseño"],
+    image: ResponsiveWebDesignImage,
+  },
+  {
+    id: "3",
+    title: "JavaScript Algorithms and Data Structures",
+    institution: "freeCodeCamp",
+    date: "Diciembre 2023",
+    hours: 300,
+    knowledge: ["JavaScript"],
+    image: JavaScriptImage,
+  },
+  {
+    id: "2",
+    title: "Ganadora de “Diseña tu plan de Empresa. Taller de emprendimiento",
+    institution: "Acepcor",
+    date: "Noviembre 2022",
+    hours: 30,
+    image: AcepcorImage,
+  },
+  {
+    id: "1",
+    title: "Curso de Introducción al Desarrollo Web: HTML y CSS (1 y 2)",
+    institution: "Google Actívate",
+    date: "Octubre 2022",
+    hours: 80,
+    knowledge: ["HTML", "CSS", "Principios de diseño"],
+    image: GoogleActivate1Image,
+    image2: GoogleActivate2Image,
+  },
+];
